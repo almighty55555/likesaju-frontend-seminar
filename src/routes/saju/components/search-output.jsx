e34e80a83117537a00681b2fcdd7a82129086a25
최주현 <wngus0223@naver.com>
2024-09-09 23:51:16 +0900

    feat/ai

--- src/routes/saju/components/search-output.jsx
+++ src/routes/saju/components/search-output.jsx
@@ -1,14 +1,17 @@
-const SajuSearchOutput = ({ outputs }) => {
-  const { name, headline, content } = outputs;
+import { useSelector } from "react-redux";
+
+const SajuSearchOutput = () => {
+  const name = useSelector((state) => state.user.nickname);
+  const outputs = JSON.parse(localStorage.getItem("sajuResult")).generalFortune[0];
 
   return (
     <div className="w-full h-full flex flex-col justify-start items-center text-justify gap-[30px] py-[70px] px-20">
-      <div className=" w-full text-neutral-800 text-[36px] font-extrabold nanum-extra-bold  pb-2 border-b border-neutral-500">
-        <span className="text-[#4a3aff]">{name}</span>님의 오늘 운세
+      <div className=" w-full text-neutral-800 text-[36px] font-extrabold nanum-extra-bold pb-2 border-b border-neutral-500">
+        <span className="text-[#4a3aff]">{name}</span> 님의 오늘 운세
       </div>
-      <div className="text-[26px] font-semibold">{headline}</div>
-      <div className="text-[22px] font-normal leading-[44px] font-PretendardRegular">
-        {content}
+      <div className="text-[26px] font-semibold self-start">{outputs.headline}</div>
+      <div className="text-[22px] font-normal leading-[44px] font-PretendardRegular self-start">
+        {outputs.content}
       </div>
     </div>
   );
--- src/routes/saju/pages/saju-page.jsx
+++ src/routes/saju/pages/saju-page.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SajuSearch from '../components/search-template';
 import { SearchOutputTheme } from '../components/search-output-theme';
 
 const SajuPage = () => {
+  const [hasSaju, setHasSaju] = useState(false);
+
   return (
     <div className="w-full py-24 bg-gradient-to-b from-neutral-400 to-neutral-200">
-      <SajuSearch />
-      <SearchOutputTheme />
+      <SajuSearch setHasSaju={setHasSaju} />
+      {hasSaju && <SearchOutputTheme />}
     </div>
   );
 };
